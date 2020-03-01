a76e27053557eac1a3302e0fe1bef966b9fbb676
cli60023 <60617076+cli60023@users.noreply.github.com>
2020-03-01 17:20:16 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Fixit9CanLiuu.appstudio
+ 
---
+++ forms/membership/membership.js
@@ -0,0 +1,17 @@
+
+let members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
+//console.log(member)
+
+let memberSign = []
+
+
+Button1.onclick=function(){
+
+var i = 0
+for (i;i<members.length;i++{
+  if(members[i] == inptFirstName.value){
+  memberSign.push(inptFirstName.value)
+  }
+}
+
+if (memberSign.length
